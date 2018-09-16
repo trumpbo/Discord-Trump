a26030f4f867555099f531ef04f126131b57109e
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-09-16 13:12:55 +1000

    i guess there should be an error message

--- trump.js
+++ trump.js
@@ -16,16 +16,20 @@ client.on("message", function(message) {
 	if (message.author.bot || !message.guild) return;
 	if (message.content === "/join") {
 		if (message.member.voiceChannel) {
-			message.member.voiceChannel.join().catch(console.error);
-			client.user.setActivity(client.voiceConnections.size + " Trump" + (client.voiceConnections.size === 1 ? "" : "s")).catch(console.error);
+			message.member.voiceChannel.join().then(function() {
+				client.user.setActivity(client.voiceConnections.size + " Trump" + (client.voiceConnections.size === 1 ? "" : "s")).catch(console.error);
+			}).catch(function() {
+				message.channel.send("I need permission to join your voice channel! Believe me, it's true.").catch(console.error);
+			});
 		} else {
 			message.channel.send("Join a voice channel! You won't regret it, believe me.").catch(console.error);
 		}
 	} else if (message.content === "/leave") {
 		const connection = message.guild.voiceConnection;
 		if (connection) {
-			connection.disconnect();
-			client.user.setActivity(client.voiceConnections.size + " Trump" + (client.voiceConnections.size === 1 ? "" : "s")).catch(console.error);
+			connection.disconnect().then(function() {
+				client.user.setActivity(client.voiceConnections.size + " Trump" + (client.voiceConnections.size === 1 ? "" : "s")).catch(console.error);
+			});
 		}
 	} else {
 		const connection = message.guild.voiceConnection;
